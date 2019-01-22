53456b84b3c656fd15a79f4d440a96685a3526f7
Fergus Leahy <fergus.leahy@gmail.com>
2019-01-22 11:30:06 +0000

    Updated AI using behaviour trees

--- Source/Ardan/ArdanGameMode.cpp
+++ Source/Ardan/ArdanGameMode.cpp
@@ -4,11 +4,20 @@
 #include "ArdanGameMode.h"
 #include "ArdanPlayerController.h"
 #include "ArdanCharacter.h"
+#include "DejaVuStateBase.h"
 
 AArdanGameMode::AArdanGameMode()
 {
 	// use our custom PlayerController class
 	PlayerControllerClass = AArdanPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<AGameStateBase> GameStateBPClass (TEXT("/Game/DejaVu/Blueprints/DejaVuState"));
+	if (GameStateBPClass.Class != NULL)
+	{
+		GameStateClass = GameStateBPClass.Class;
+	}
+	//GameStateClass = ADejaVuStateBase::StaticClass();
 
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
---
+++ Source/Ardan/DejaVuStateBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameStateBase.h"
+#include "DejaVuStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARDAN_API ADejaVuStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
